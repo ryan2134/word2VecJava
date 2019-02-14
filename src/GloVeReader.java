a734c6c61b7ca1dea76611780bf32e764b08de23
ryan2134 <ryancheng2134@gmail.com>
2019-02-15 02:44:43 +1100

    Need to fix up the addition and subtraction with 2 words, fix code and clean code, add more functionality

--- src/GloVeReader.java
+++ src/GloVeReader.java
@@ -7,33 +7,37 @@ import java.util.Scanner;
 
 public class GloVeReader {
     public static void main(String[] args) throws IOException {
+        String input;
         Scanner scanner = new Scanner(new FileReader("glove.6B.50d.txt"));
         VectorEmbedding vEmbedding = new VectorEmbedding();
         HashMap<String, Float[]> v = new HashMap<>();
         // Just to check how many wordVectors have gone through the system
         int j = 0;
-        while (scanner.hasNextLine()) {
-            // split space between word and float number embedding
+        while (scanner.hasNextLine() ) {
+            // Split space between word and float number embedding
             String[] words = scanner.nextLine().split(" ");
             // Array of Floats which will keep as values for words
-            // Because we are not going to store word as its value
             Float[] values = new Float[words.length - 1];
             for( int i = 1; i < words.length; i++){
                 values[i - 1] = Float.parseFloat(words[i]);
             }
 
             // Now all the values are stored in array, store it in the hashMap.
-            HashMap<String, Float[]> x = new HashMap<>();
-            x.put(words[0],values);
-            vEmbedding.add(x);
+//            HashMap<String, Float[]> x = new HashMap<>();
+//            x.put(words[0],values);
+            vEmbedding.put(words[0],values);
             j++;
         }
+        input = getInput();
+        vEmbedding.query(input);
         //vEmbedding.printVectors();
+    }
+
+    private static String getInput() {
         System.out.println("Type a word to find words with similar meaning (type exit to finish simulation)");
         Scanner sc = new Scanner(System.in);
         String input = "";
-        input = sc.next();
-        vEmbedding.query(input);
-
+        input = sc.nextLine();
+        return input;
     }
 }
--- src/VectorEmbedding.java
+++ src/VectorEmbedding.java
@@ -3,48 +3,82 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 
-public class VectorEmbedding {
-    private ArrayList<HashMap<String, Float[]>> vectorList;
+public class VectorEmbedding extends HashMap<String, Float[]>{
 
-    public VectorEmbedding(){
-        this.vectorList = new ArrayList<>();
-    }
-
-    public void add(HashMap<String, Float[]> v){
-        this.vectorList.add(v);
-    }
-
-    public void query(String word){
-        HashMap<String, Float[]> query = new HashMap<>();
-        for (HashMap<String, Float[]> hashMap : vectorList) {
-            //System.out.println(hashMap.keySet().toArray()[0]);
-            if(hashMap.containsKey(word)){
-//                System.out.println("Found word");
-                query = hashMap;
-                //System.out.println(hashMap.get(word));
-                break;
-            }
+    public Float[] sumVectors(Float[] v1, Float[] v2){
+        int length = v1.length;
+        Float[] values = new Float[length];
+        for(int i = 0; i < length; i++){
+            values[i] = v1[i] + v2[i];
         }
-        gatherCosSim(query);
-        // Print stuff here:
-
-
+        System.out.println(Arrays.toString(values));
+        return values;
     }
-    public void gatherCosSim(HashMap<String, Float[]> query){
+
+    public Float[] subtractVectors(Float[] v1, Float[] v2){
+        int length = v1.length;
+        Float[] values = new Float[length];
+        for(int i = 0; i < length; i++){
+            values[i] = v1[i] - v2[i];
+        }
+        System.out.println(Arrays.toString(values));
+        return values;
+    }
+
+    public void query(String input){
+        String[] words = input.split(" ");
+        System.out.println(Arrays.toString(words));
+        HashMap<String, Float[]> query = new HashMap<>();
+        if(words.length == 1){
+            if(this.containsKey(input)){
+                System.out.println("Found word");
+                query.put(input, this.get(input));
+            }
+        } 
+        else if (words.length == 3 && this.containsKey(words[1]) && this.containsKey(words[2])){
+            String newWord = words[1] + words[2];
+            Float[] newValues;
+            if (words[0].equals("add")){
+                newValues = sumVectors(this.get(words[1]), this.get(words[2]));
+            }
+            else if (words[0].equals("subtract")){
+                newValues = subtractVectors(this.get(words[1]), this.get(words[2]));
+            }
+            else{
+                System.out.println("Wrong keyword entered");
+                return;
+            }
+            query.put(newWord, newValues);
+        }
+        else{
+            System.out.println("Too many words have been entered.");
+            return;
+        }
+        gatherCosSim(query, this);
+    }
+
+    public void gatherCosSim(HashMap<String, Float[]> query, VectorEmbedding v){
         ArrayList<Float> nums = new ArrayList<>();
+//        int length = query.get(query.keySet().toArray()[0]).length;
+//        System.out.println(length);
+//        HashMap<String, Float[]> currentVector = new HashMap<>();
         float current;
         HashMap<Float, String> table = new HashMap<>();
-        for (HashMap<String, Float[]> hashMap : vectorList) {
+        for (String word : v.keySet()) {
             //System.out.println(hashMap.keySet());
-            if(hashMap.containsKey(query.keySet().toArray()[0])){
+
+            /* wtf */
+            if(word.contains((String) (query.keySet().toArray()[0]))){
+                System.out.println("hello");
                 continue;
             }
-            //System.out.println(query.keySet().toArray()[0]);
-            current = cosineSimilarity(query.get(query.keySet().toArray()[0]), hashMap.get(hashMap.keySet().toArray()[0]));
+            /* wtf */
 
+            //System.out.println(query.keySet().toArray()[0]);
+            current = cosineSimilarity(query.get(query.keySet().toArray()[0]), v.get(word));
             //System.out.println(current);
             nums.add(current);
-            table.put(current, (String) hashMap.keySet().toArray()[0]);
+            table.put(current, word);
             //System.out.println("Word: " + table.get(current));
         }
         Float test[] = query.get(query.keySet().toArray()[0]);
@@ -62,9 +96,7 @@ public class VectorEmbedding {
     }
 
     // Assuming that the vectors will of the same length as each other
-    public static Float cosineSimilarity(Float[] vectorA, Float[] vectorB) {
-        //System.out.println(Arrays.toString(vectorA));
-
+    public Float cosineSimilarity(Float[] vectorA, Float[] vectorB) {
         float dotProduct = 0;
         float normA = 0;
         float normB = 0;
